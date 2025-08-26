c90c26555dd0b1c0dadeadc745514e9053e4f13a
Bichoyyzzz <bichoy.ghatas04@gmail.com>
2025-08-26 14:39:27 +0200

    Delete Cleanup-90Days.ps1

--- Cleanup-90Days.ps1
+++
@@ -1,3 +0,0 @@
-Connect-MgGraph -Scopes "DeviceManagementManagedDevices.ReadWrite.All","Device.ReadWrite.All"
-Select-MgProfile v1.0
-Invoke-IntuneCleanup -OlderThanDays 90 -OperatingSystem @('Windows','macOS') -AlsoRemoveFromEntraId -WhatIf
