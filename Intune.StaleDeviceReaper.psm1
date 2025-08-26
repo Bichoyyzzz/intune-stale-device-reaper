8cdb99c8925aa88abef83792f8b192da4a511ca3
Bichoyyzzz <bichoy.ghatas04@gmail.com>
2025-08-26 14:39:52 +0200

    Delete Intune.StaleDeviceReaper.psm1

--- Intune.StaleDeviceReaper.psm1
+++
@@ -1,4 +0,0 @@
-. $PSScriptRoot/Private/Invoke-WithGraphRetry.ps1
-. $PSScriptRoot/Private/Connect-GraphApp.ps1
-Get-ChildItem -Path $PSScriptRoot/Public/*.ps1 | ForEach-Object {. $_.FullName }
-Export-ModuleMember -Function Get-StaleIntuneDevice,Remove-IntuneDevice,Invoke-IntuneCleanup
